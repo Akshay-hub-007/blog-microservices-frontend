9fb2745c97fd19accf1e309e11d2440e25b87350
Akshay-hub-007 <akshaykalangi9@gmail.com>
2025-11-24 02:01:23 +0530

    enhance blog page with detailed view, author info, and comment section

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,11 +1,100 @@
-import React from 'react'
+"use client"
+import Loading from '@/components/Loading'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Blog, blog_service, useAppData, User } from '@/context/AppContext'
+import axios from 'axios'
+import { Bookmark, Edit, Trash2 } from 'lucide-react'
+import Link from 'next/link'
+import { useParams, useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
 
-const page = () => {
+const BlogPage = () => {
+  const { isAuth, user } = useAppData()
+  const { id } = useParams()
+
+  const [blog, setBlog] = useState<Blog | null>(null)
+  const [author, setAuthor] = useState<User | null>()
+  const [loading, setLoading] = useState(false)
+  const router = useRouter()
+  async function fetchBlogs() {
+    try {
+      setLoading(true)
+      const { data } = await axios.get(`${blog_service}/api/v1/blog/${id}`)
+      setBlog(data.blog)
+      setAuthor(data.author)
+      console.log(data)
+    } catch (error) {
+      console.log("Error in fetching blog", error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchBlogs()
+  }, [id])
+
+  if (!blog) {
+    return <Loading />
+  }
   return (
-    <div>
-      
+    <div className='max-w-4xl mx-auto p-6 space-y-6'>
+      <Card>
+        <CardHeader>
+          <h1 className='text-3xl font-bold text-gray-900'>{blog.title}</h1>
+          <p className='text-gray-600 mt-2 flex items-center'>
+            <Link href={`/profile/${author?._id}`} className='flex items-center gap-2'>
+              <img src={blog.image} className='w-8 h-8 rounded-full' alt="" />
+              {author?.name}
+            </Link>
+            {
+              isAuth && <Button variant={"ghost"} className='mx-3' size={"lg"}> <Bookmark /></Button>
+            }
+            {
+              blog.author === user?._id && (
+                <>
+                  <Button size={"sm"} onClick={() => router.push(`/blog/edit/${blog.id}`)}>
+                    <Edit />
+                  </Button>
+
+                  <Button variant={"destructive"} className='ml-2' size={"sm"}>
+                    <Trash2 />
+                  </Button>
+                </>
+              )
+            }
+          </p>
+        </CardHeader>
+        <CardContent>
+          <img src={blog.image} alt={blog.title}
+            className='w-full h-64 object-cover rounded-lg mb-4' />
+
+          <p className='text-lg text-gray-700 mb-4'>{blog.description}</p>
+          <div className='prose max-w-none' dangerouslySetInnerHTML={{ __html: blog.blogcontent }}>
+
+          </div>
+        </CardContent>
+      </Card>
+      {
+        isAuth && <Card>
+          <CardHeader className='text-xl font-semibold'>Leave a comment</CardHeader>
+          <CardContent>
+            <Label htmlFor='comment'>Your Comment</Label>
+            <Input
+              id='comment'
+              className='my-2'
+              placeholder='Type Your Comment here'
+            />
+            <Button>Post Comment</Button>
+          </CardContent>
+        </Card>
+
+      }
     </div>
   )
 }
 
-export default page
+export default BlogPage
